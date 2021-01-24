1d8d2ea8916a6629c7fb69a3804844ab3715d30d
Brendon Mota <bmota@its.jnj.com>
2021-01-24 15:56:03 -0300

    Adding CORS

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const user = require('./src/routes/user');
 const schedule = require('./src/routes/schedule');
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.use('/user', user);
 app.use('/schedule', schedule);
