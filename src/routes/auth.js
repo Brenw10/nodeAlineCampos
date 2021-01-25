09bb7c74649bb72f28984188cb969722d3fa068a
Brendon Mota <bmota@its.jnj.com>
2021-01-25 11:44:52 -0300

    added try catch for auth

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,11 +4,15 @@ const auth = require('../services/auth')
 const router = express.Router();
 
 router.use(async function (req, res, next) {
-  const user = await auth.getUser(req.body.idToken);
-  if (user.sub) {
-    res.locals.user = user;
-    next();
-  } else {
+  try {
+    const user = await auth.getUser(req.body.idToken);
+    if (user.sub) {
+      res.locals.user = user;
+      next();
+    } else {
+      res.sendStatus(401);
+    }
+  } catch {
     res.sendStatus(401);
   }
 });
