b5a90a44ff7bfd16be041f5c17e97be67b0df7e4
Brendon Mota <bmota@its.jnj.com>
2021-02-01 18:27:30 -0300

    Changed mongoose connect path

--- server/src/config/database.dist.js
+++ server/src/config/database.dist.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.connect(
-  'mongodb://localhost:27017/alinecampos',
+  'mongodb://mongodb:27017/alinecampos',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
