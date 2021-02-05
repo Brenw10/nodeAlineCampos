92b9b98523dbd62725b71ca8c239f19f5af3e6d9
Brendon Mota <bmota@its.jnj.com>
2021-02-05 12:04:22 -0300

    Enable .env file

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 mongoose.connect(
   'mongodb://mongodb:27017/alinecampos',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }
+    auth: { authSource: "admin" },
+    user: process.env.MONGO_INITDB_ROOT_USERNAME,
+    pass: process.env.MONGO_INITDB_ROOT_PASSWORD,
+  },
 );
+
 
 module.exports = mongoose;
