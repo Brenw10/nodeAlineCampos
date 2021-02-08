ee27309e6c23f6e442a60a9d5429336d59b304ce
Brendon Mota <bmota@its.jnj.com>
2021-02-08 13:16:27 -0300

    sort coupon by creationg desc

--- server/src/services/coupon.js
+++ server/src/services/coupon.js
@@ -4,7 +4,7 @@ const user = require('../services/user');
 async function getAll(sub) {
   const currentUser = await user.get(sub);
   if (!currentUser.admin) return Promise.reject();
-  return CouponModel.find();
+  return CouponModel.find().sort({ _id: -1 });
 }
 
 async function remove(sub, _id) {
